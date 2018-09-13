9872fab6f4192e3456440ad48d2e44f47b813d1e
Gage <gagedark@gmail.com>
2018-09-13 17:34:54 +0800

    add style for login page

--- src/containers/LoginContainer.js
+++ src/containers/LoginContainer.js
@@ -12,20 +12,75 @@ class LoginContainer extends Component {
   }
   render () {
     return (
-      <form onSubmit={this._createUser}>
-        <TextField
-          required
-          type='email'
-          label='email'
-          onChange={this._updateUserName} />
-        <Button
-          variant='contained'
-          color='primary'
-          size='large'
-          type='submit'>
-          Create a new wallet
-        </Button>
-      </form>
+      <div>
+        <header>
+          <h1>Bet.tr</h1>
+          <h2>Make bets on a better you</h2>
+        </header>
+        
+        <div className="home-cover">
+          <img width="300" src="https://cdn.dribbble.com/users/754899/screenshots/3662293/fantasy_antelope.gif"/>
+        </div>
+        <form className="login-form" onSubmit={this._createUser}>
+          <TextField
+            required
+            type='email'
+            label='Enter your email'
+            className='login-input'
+            onChange={this._updateUserName} />
+          
+          <Button
+            variant='contained'
+            className='login-button'
+            color='primary'
+            size='large'
+            type='submit'>
+            Create a new wallet
+          </Button>
+        </form>
+        <style jsx>{`
+        .login-form{
+          text-align: center;
+        }
+        :global(.login-input){
+          display: flex;
+          margin: 0 auto 20px;
+          width: 232px;
+          border: 1px solid #E0E0E0;
+          border-radius: 2px;
+          background: #fff;
+        }
+        :global(.login-input input){
+
+        }
+        :global(.login-button){
+          font-family: 'Comfortaa', cursive;
+          background: #FFE018;
+          color: #010311;
+        }
+        header{
+          text-align: center;
+        }
+        header h1{
+          font-size: 60px;
+          margin-bottom: 10px;
+        }
+        header h2{
+          font-size: 16px;
+          font-weight: normal;
+        }
+        .home-cover{
+          text-align: center;
+        }
+        :global(body) {
+          background: #010311;
+          font-family: 'Comfortaa', cursive;
+          margin: 0px;
+          padding: 0px;
+          color: #fff;
+        }
+        `}</style>
+      </div>
     )
   }
   _updateUserName = (e) => {
